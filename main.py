76780d05406bb3632e934baaffe9231024b9b7df
burger4d <104983707+burger4d@users.noreply.github.com>
2022-06-06 11:49:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from krevetka import *
 from tkinter import *
 from time import sleep
 from online_sources import *
-import os
 tk = Tk()
 tk.title("Chess")
 tk.configure(cursor = "hand2")
@@ -11,55 +10,74 @@ w.pack()
 Move = ""
 phase = 0
 lichess_url = None
+lvl = 0
+
 def Undo():
+    """undo a move"""
     global coord
     try:
-        coord.pop()
         coord.pop()
     except:
         pass
     Draw()
 
-
-def select1():
-    global phase, Btn1, Btn2, Btn3, undo
-    enter.destroy()
-    phase = 1
+def global_select():
+    global Btn1, Btn2, Btn3, undo, lvl, level, enter, li_option
     Btn1.destroy()
     Btn2.destroy()
     Btn3.destroy()
+    lvl = level.get()
+    level.destroy()
+    if IsLichessGame.get()=="on":
+        enter.pack()
+    li_option.destroy()
     undo = Button(tk, text="undo", overrelief="ridge", command=Undo)
     undo.pack()
+    
+def select1():
+    """human vs human"""
+    global phase, enter
+    enter.destroy()
+    global_select()
+    phase = 1
     Draw()
 
 
 def select2():
-    global phase, Btn1, Btn2, Btn3, undo, enter
+    """human(White) vs Ai(Black)"""
+    global phase
     phase = 2
-    Btn1.destroy()
-    Btn2.destroy()
-    Btn3.destroy()
-    undo = Button(tk, text="undo", overrelief="ridge", command=Undo)
-    undo.pack()
-    enter.pack()
+    global_select()
     Draw()
 
 
 def select3():
-    global phase, Btn1, Btn2, Btn3, undo, enter
+    """AI(White) vs human(Black)"""
+    global phase, lvl
+    phase = 3 
+    global_select()
+    play_white(coord, lvl)
+    Draw()
+
+def select4():
+    """for the tests"""
+    global phase, Btn1, Btn2, Btn3, Btn4, lvl, level
     phase = 3
     Btn1.destroy()
     Btn2.destroy()
     Btn3.destroy()
-    Draw()
-    play_white(coord)
-    undo = Button(tk, text="undo", overrelief="ridge", command=Undo)
-    undo.pack()
-    enter.pack()
-    Draw()
-
+    Btn4.destroy()
+    lvl = level.get()
+    level.destroy()
+    while 1:
+        Draw()
+        play_white(coord, lvl)
+        Draw()
+        play_black(coord, lvl)
+        
 
 def click(event):
+    """getting the position of the click and moving pieces"""
     global Move
     if phase != 0:
         Draw()
@@ -85,65 +103,70 @@ def click(event):
             Move = ""
             Draw()
             if phase == 2 and test != coord:
-                play_black(coord)
+                play_black(coord, lvl)
                 Draw()
             elif phase == 3 and test != coord:
-                play_white(coord)
+                play_white(coord, lvl)
                 Draw()
 
 
 def select_url(event):
-    #lichess game
+    """if this is a lichess game"""
     global lichess_url, enter
     lichess_url = enter.get()
     enter.destroy()
-    w.create_text(320, 320, text="5s for the mouse to be in the top left-hand corner of the board", fill="green", tag="url")
-    tk.update()
+    sec=5
+    while sec>0:
+        w.create_text(320, 320, text=str(sec)+"s for the mouse to be in the\ntop left-hand\ncorner of the board", fill="green", font="Times 20", tag="url")
+        tk.update()
+        sleep(1)
+        sec-=1
+        w.itemconfigure("url", state="hidden")
     coord_board1 = get_mouse_board()
-    w.itemconfigure("url", state="hidden")
-    w.create_text(320, 320, text="5s for the mouse to be in the lower right-hand corner of the board", fill="green", tag="url")
-    tk.update()
+    sec=5
+    while sec>0:
+        w.create_text(320, 320, text=str(sec)+"s for the mouse to be in the\nlower right-hand\ncorner of the board", fill="green", font="Times 20", tag="url")
+        tk.update()
+        sleep(1)
+        sec-=1
+        w.itemconfigure("url", state="hidden")
     coord_board2 = get_mouse_board()
-    w.itemconfigure("url", state="hidden")
     coord_board = coord_board1+coord_board2
     old_data = []
     if phase == 3:
         play_mouse(str(coord.move_stack[-1]), coord_board, False)
     while True:
         if phase == 2:
-            #si ia joue les noirs...
             data = lichess_board(lichess_url)
-            #print(data)
             if len(data)%2!=0 and data!=old_data:
-                #print(data[-1])
                 play_human(data[-1])        
                 Draw()
                 old_data = data
-                print("ia noir va jouer")
-                play_black(coord)
+                print("Krevetka(Black) is thinking")
+                play_black(coord, lvl)
                 play_mouse(str(coord.move_stack[-1]), coord_board, coord.turn)
                 Draw()
             else:
-                print("en attente(le blancs doivent jouer)")
-                #print(data)
-                sleep(2)
+                print("waiting(White must play)")
+                sleep(1)
         elif phase == 3:
             data = lichess_board(lichess_url)
             if len(data)%2==0 and data!=old_data:
                 play_human(data[-1])
                 Draw()
                 old_data = data
-                print("ia blanc va jouer")
-                play_white(coord)
+                print("Krevetka(White) is thinking...")
+                play_white(coord, lvl)
                 play_mouse(str(coord.move_stack[-1]), coord_board, coord.turn)
                 Draw()
             else:
-                print("en attente(les noirs doivent jouer)")
-            sleep(2)
+                print("waiting(Black must play)")
+            sleep(1)
         else:
             break
 
 def Draw():
+    """'drawing' the board"""
     w.itemconfigure("pieces", state="hidden")
     d = string_coord(coord)
     if phase == 3:
@@ -159,6 +182,7 @@ def Draw():
     tk.update()
 
 def verification():
+    """verify if there is a checkmate, a draw..."""
     global phase
     if coord.is_checkmate():
         phase = 0
@@ -177,7 +201,7 @@ def verification():
     elif coord.can_claim_draw():
         w.create_text(320, 320, text="DRAW?", font="Times 30", fill="orange")
 
-for i in range(1, 9, 2):  # creation du plateau en interface
+for i in range(1, 9, 2):  # the squares of the board
     for j in range(1, 9, 2):
         w.create_rectangle(i*80, (j-1)*80, (i+1)*80, j*80, fill="#cccccc")
 for i in range(0, 9, 2):
@@ -194,8 +218,15 @@ Btn2.pack()
 Btn3 = Button(tk, text="AI vs human", overrelief="ridge", command=select3)
 Btn3.pack()
 enter = Entry(tk)
-#Btn4 = Button(tk, text="learn", overrelief="ridge", command=select4)
+#Btn4 = Button(tk, text="test", overrelief="ridge", command=select4)
 #Btn4.pack()
+IsLichessGame = StringVar()
+li_option = Checkbutton(tk, text="Lichess Game", variable=IsLichessGame, onvalue="on", offvalue="off")
+IsLichessGame.set("off")
+li_option.pack()
+level = Scale(tk, orient="horizontal", from_=0, to=3, resolution=1, tickinterval=10, length=100, label="level(depth)")
+level.pack()
+level.set(3)
 enter.bind("<Return>", select_url)
 w.bind_all("<Button-1>", click)
 tk.mainloop()
